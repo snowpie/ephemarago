2571e49d90f556ccc680d0c878cda0bc5d5f4289
snowpie <github@antonyking.co.uk>
2025-06-13 15:09:38 +0100

    Added some tests for secrets

--- secret_test.go
+++ secret_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"assert/assert"
-	
+	"fmt"
 	"testing"
 )
 func TestSecret(t *testing.T) {
@@ -13,4 +13,21 @@ func TestSecret(t *testing.T) {
 	assert.Equal("second secret", t, secrets["second"](), "second secret")
 	assert.NotEqual("first secret should not equal second secret", t, secrets["first"](), secrets["second"]())
 	assert.NotEqual("second secret should not equal first secret", t, secrets["second"](), secrets["first"]())
+}
+
+func TestStachSecret(t *testing.T) {
+	fmt.Println(Secrets)
+	key1 := stashsecret("first secret")
+	key2 := stashsecret("second secret")
+	fmt.Println(Secrets)
+
+	assert.NotEqual("first key should not equal second secret", t, key1, key2)
+
+	assert.Equal("first secret retrieved correctly", t, getsecret(key1), "first secret")
+	assert.Equal("second secret retrieved correctly", t, getsecret(key2), "second secret")
+	
+	assert.Equal("first secret stored in backend", t, Secrets[key1](), "first secret")
+	assert.Equal("second secret stored in backend", t, Secrets[key2](), "second secret")
+	
+	
 }
